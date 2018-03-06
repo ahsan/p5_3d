5387924f87b5cfa13b5a91c0ab78891a8a21a9a2
Abdullah Ahsan <abdullah.ahsan@xgrid.co>
2018-03-06 19:02:38 +0500

    Multiple switches
    Signed-off-by: Abdullah Ahsan <abdullah.ahsan@xgrid.co>

--- sketch.js
+++ sketch.js
@@ -4,73 +4,151 @@ var yPos = 0;
 var zPos = 600;
 var img;
 
+var sw;
+var switches = [];
+
 function preload() {
   img = loadImage('./front.png');
+  typeA = loadImage('./typeA.png');
+  typeB = loadImage('./typeB.png');
 }
 
 
 function setup() {
-  createCanvas(1000, 800, WEBGL);
+  var canvas = createCanvas(1000, 800, WEBGL);
+  canvas.parent('sketch-holder');
+
+  // sw = new
+
+  for (var i=0; i<=7; i++){
+    switches.push(
+      new Switch(i*100 - 400, 0, 0, 60, 300, 150, typeB)
+    );
+  }
 }
 
 function draw() {
-  camera(0, 0, 500, 0, 0, 0, 0, 1, 0);
+  // camera(0, 0, 500, 0, 0, 0, 0, 1, 0);
   // camera(map(mouseX, 0, width, 300,-300), map(mouseY, 0, width, 250, -250), zPos, 0, 0, 0, 0, 1, 0);
   // pointLight(255, 255, 255, 0, 0, 100)
   ambientLight(255);
 
   background(120);
 
+  switches.forEach(sw => {
+    sw.draw();
+  });
 
-  push();
-  translate(0, 0, -150);
-  noStroke();
-  // strokeWeight(1)
-  // stroke(0)
-  fill(255)
-  plane(300, 300);
-  pop();
+  // sw.draw()
 
-  push();
-  translate(0, 150, 0)
-  rotateX(PI/2);
-  noStroke();
-  // strokeWeight(1)
-  // stroke(0)
-  fill(255)
-  plane(300, 300)
-  pop();
+  // pointLight(255, 255, 255, 0, 0, 100)
+}
 
-  var boxWidth = 175;
-  var boxHeight = 15;
+function getSwitch(x, y){
+  console.log('inside get switch')
+  switches.forEach(sw => {
+    if(sw.pointInsideFace(x, y)) {
 
-  push();
-  fill(0);
-  stroke(0)
-  translate(xPos, yPos, 0);
-  box(boxWidth, boxHeight, 100)
-  pop();
+      console.log('found switch')
+      console.log(sw)
+      return sw.x;
+    }
 
-  push();
-  texture(img);
-  translate(xPos, yPos, 51);
-  plane(boxWidth, boxHeight);
-  pop();
+  });
+}
 
-  pointLight(255, 255, 255, 0, 0, 100)
+function mouseDragged(event) {
+  // xPos = map(event.clientX, 0, width, -300, 300);
+  // yPos = map(event.clientY, 0, height, -300, 300);
+  // sw.move(mouseX - (width/2), mouseY - (height/2));
 }
 
 
-function mouseDragged(event) {
-  // var z = event.clientY
-  // zPos = map(z, 0, height, 1000, 100);
-  // xPos = event.clientX;
-  // yPos = event.clientY;
+function keyPressed(event) {
+  sw.moveZ(-100)
+}
 
-  xPos = map(event.clientX, 0, width, -300, 300);
-  yPos = map(event.clientY, 0, height, -300, 300);
+function mousePressed() {
+  // sw.moveZ(0)
+  // console.log(sw.pointInsideFace(mouseX-(width/2), mouseY-(height/2)))
+  console.log(getSwitch(mouseX, mouseY))
+}
 
 
+/**
+ * Switch Class
+ * @param {float} x
+ * @param {float} y
+ * @param {float} z
+ * @param {float} width
+ * @param {float} height
+ * @param {float} depth
+ * @param {image} txtr
+ */
+function Switch(x, y, z, width, height, depth, txtr) {
+
+  // Position
+  this.xPos = x;
+  this.yPos = y;
+  this.zPos = z;
+
+  // Intended position
+  this.intendedZ = this.zPos;
+
+  // Size
+  this.width = width;
+  this.height = height;
+  this.depth = depth;
+
+  this.txtr = txtr;
+
+
+  // Draw method
+  this.draw = function(){
+
+    this.updatePosition();
+
+    push();
+    translate(this.xPos, this.yPos, this.zPos - (this.depth/2));
+
+    // box
+    noStroke();
+    fill(50);
+    box(this.width, this.height, this.depth);
+
+
+    // front panel
+    translate(0, 0, this.depth/2);
+    fill(0);
+    texture(this.txtr);
+    plane(width, height);
+    pop();
+  }
+
+  // Move box around
+  this.moveZ = function(zPos) {
+    this.intendedZ = zPos;
+  }
+  this.move = function(xPos, yPos) {
+    this.xPos = xPos;
+    this.yPos = yPos;
+  }
+
+  // Update position function
+  this.updatePosition = function() {
+    // TODO: Add an anchor condition
+    this.zPos = lerp(this.zPos, this.intendedZ, 0.08);
+  }
+
+  // log position
+  this.logPosition = function() {
+    console.log(this.xPos, this.yPos, this.zPos)
+  }
+
+  // pointInside
+  this.pointInsideFace = function(pointX, pointY) {
+    return (pointX >= this.xPos-this.width/2 && pointX <= this.xPos + this.width/2 && pointY >= this.yPos-this.height/2 && pointY <= this.yPos + this.height/2)
+  }
 }
 
 
